81a6da0aade226b2f61822e650105e6f9697cc74
itsjbongit <jeromebiju2023@gmail.com>
2026-06-12 19:34:00 +0530

    New File Add

---
+++ keep_alive.py
@@ -0,0 +1,19 @@
+# keep_alive.py
+import os
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot is alive and processing reminders!"
+
+def run():
+    # Render automatically allocates a PORT variable
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
